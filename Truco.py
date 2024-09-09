81a59ea7724b2c37818dcddb3ee1fe7e300320e3
Henrique Bonazzi <hidabonazzi@gmail.com>
2024-09-09 18:07:30 -0300

    finished hide and highest card

--- Truco.py
+++ Truco.py
@@ -3,11 +3,13 @@ import random
 #Card organization
 cards = { 70: ["3", "3", "3", "3"], 60 : ["2", "2", "2", "2"], 50 : ["A", "A", "A", "A"],
          40 : ["K", "K", "K", "K"], 30 : ["J", "J", "J", "J"], 
-        20 : [ "Q",  "Q",  "Q", "Q"], 10 : ["7", "7", "7", "7"]}
+        20 : [ "Q",  "Q",  "Q", "Q"], 10 : ["7", "7", "7", "7"], 0: []}
+
 shuffler = []
 for lst in cards.values():
     for card in lst:
         shuffler.append(card)
+
 
 #Game
 score_list = [0,0]
@@ -31,13 +33,11 @@ class Game:
         Game.player_count += 1
         self.id = Game.player_count
         
-    def highest_card(self):
-        biggest_card = shuffler[6]
-        lst_key_bcard = [key for key, value in cards.items() if biggest_card in value]
-        for key_bcard in lst_key_bcard:
-            cards[80] = cards[key_bcard]
-            del cards[key_bcard]
+    def highest_card():
+        pass
     
+    def __repr__():
+        return Game.highest_card
     def play_card(self, name):
         self.name = name
         if self.id == 1:
@@ -72,7 +72,9 @@ class Game:
                                 + str(player1_hand[2]) + "\n\n")
             while hide not in player1_hand:
                 hide = input("WHICH ONE OF YOUR CARDS WOULD YOU LIKE TO HIDE?\n\n")
-            choices.append(hide)
+            cards[0] = "hide"
+            choices.append("hide")
+            print(choices)
             card_index = player1_hand.index(hide)
             player1_hand.pop(card_index)
             player1_hand.append(" ")    
@@ -83,7 +85,8 @@ class Game:
                             + str(player2_hand[2]) + "\n\n"))
             while hide not in player2_hand:
                 hide = input("WHICH ONE OF YOUR CARDS WOULD YOU LIKE TO HIDE?\n\n")
-            choices.append(hide)
+            cards[0] = "hide"
+            choices.append("hide")
             card_index = player2_hand.index(hide)            
             player2_hand.pop(card_index)
             player2_hand.append(" ")
@@ -92,6 +95,7 @@ class Game:
         self.name = name
         if self.id == 1:
             pass
+
 
 name1 = input('''
 Welcome to a thrilling 2-player Truco duel! 
@@ -111,30 +115,38 @@ player2 = Game(name2)
 while GameOn:
     #Game setup
     RoundOn = True
-    random.shuffle(shuffler)
     score_list2 = [0,0]
     round_number2 = 1
-    player1_hand = [shuffler[0], shuffler[1], shuffler[2]]
-    player2_hand = [shuffler[3], shuffler[4], shuffler[5]]
     print("ROUND: " + str(round_number))
     print("\nPOINTS:\n" + player_list[0] + ": " +  str(score_list[0])
               + "\n" + player_list[1] + ": " +  str(score_list[1]))
+    #Highest card
+    biggest_card = shuffler[6]
+    lst_key_bcard = [key for key, value in cards.items() if biggest_card in value]
+    for key_bcard in lst_key_bcard:
+        cards[80] = cards[key_bcard]
+    print("THE HIGHEST CARD OF THIS ROUND IS :\n", cards[80][0])
+    #Giving out cards
+    random.shuffle(shuffler)
+    player1_hand = [shuffler[0], shuffler[1], shuffler[2]]
+    player2_hand = [shuffler[3], shuffler[4], shuffler[5]]
     while RoundOn:
-        if round_number%2: 
-            #Score System
+        if round_number%2:
+            #Play System
             if len(choices) >= 2:
                 lst_choice = [key for key, value in cards.items() if choices[0] in value]
                 lst_choice2 = [key for key, value in cards.items() if choices[1] in value]
+                lst_choice.reverse()
+                lst_choice2.reverse()
                 if lst_choice[0] > lst_choice2[0]:
                     score_list2[0] += 1
                 elif lst_choice[0] < lst_choice2[0]:
                      score_list2[1] += 1
                 else:
-                    score_list2[0] += 1
-                    score_list2[1] += 1    
+                    score_list2[0] += 1 
                 choices.pop()
                 choices.pop()
-            print("SCORES:\n" + player_list[0] + ": " +  str(score_list2[0]) #Maybe turn into function later
+            print("\nSCORES:\n" + player_list[0] + ": " +  str(score_list2[0]) #Maybe turn into function later
                 + "\n" + player_list[1] + ": " +  str(score_list2[1]))
             #Playing System
             if round_number2 % 2 == 1:
@@ -200,16 +212,17 @@ while GameOn:
             if len(choices) >= 2:
                 lst_choice = [key for key, value in cards.items() if choices[1] in value]
                 lst_choice2 = [key for key, value in cards.items() if choices[0] in value]
+                lst_choice.reverse()
+                lst_choice2.reverse()
                 if lst_choice[0] > lst_choice2[0]:
                     score_list2[0] += 1
                 elif lst_choice[0] < lst_choice2[0]:
                      score_list2[1] += 1
                 else:
-                    score_list2[0] += 1
                     score_list2[1] += 1    
                 choices.pop()
                 choices.pop()
-            print("SCORES:\n" + player_list[0] + ": " +  str(score_list2[0]) #Maybe turn into function later
+            print("\nSCORES:\n" + player_list[0] + ": " +  str(score_list2[0]) #Maybe turn into function later
                 + "\n" + player_list[1] + ": " +  str(score_list2[1])) 
             if round_number2 % 2 == 0:
                 #Winning Point System
